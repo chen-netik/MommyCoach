f73c00ffe201c2003aa7213536ed154d1af40d46
Chen Liang <leungchen921@gmail.com>
2013-06-19 12:30:38 -0400

    change home page and search bar.
    Signed-off-by: Chen Liang <leungchen921@gmail.com>

--- app/code/local/Netiksolutions/Service/Block/Html/Header.php
+++ app/code/local/Netiksolutions/Service/Block/Html/Header.php
@@ -38,7 +38,7 @@ class Netiksolutions_Service_Block_Html_Header extends Mage_Page_Block_Html_Head
     {
         if (empty($this->_data['welcome'])) {
             if (Mage::isInstalled() && Mage::getSingleton('customer/session')->isLoggedIn()) {
-                $this->_data['welcome'] = $this->__('Hi, %s', $this->escapeHtml(Mage::getSingleton('customer/session')->getCustomer()->getFirstname()));
+                $this->_data['welcome'] = $this->__('Hi %s!', $this->escapeHtml(Mage::getSingleton('customer/session')->getCustomer()->getFirstname()));
             } else {
                 $this->_data['welcome'] = Mage::getStoreConfig('design/header/welcome');
             }
